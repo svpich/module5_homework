821ce576844e9da7bc247fd395edeb4dd5552172
Святослав <69903725+svpich@users.noreply.github.com>
2023-06-09 00:03:25 +0900

    rename foreach element

--- 5.7.3_(HW-01).js
+++ 5.7.3_(HW-01).js
@@ -4,6 +4,6 @@ myMap.set("key1", "value1");
 myMap.set("key2", "value2");
 myMap.set("key3", "value3");
 
-for (const e of myMap.keys()) {
-    console.log(`Ключ — ${e}, значение — ${myMap.get(e)}`);
+for (const key of myMap.keys()) {
+    console.log(`Ключ — ${key}, значение — ${myMap.get(key)}`);
 }
